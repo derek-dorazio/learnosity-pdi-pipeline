2f6836ebc777c44726bd832257ff06fc97c346d0
Derek Dorazio <ddorazio@cainc.com>
2016-10-03 15:05:33 -0400

    Updated file paths

--- script/run_learnosity_pipeline.sh
+++ script/run_learnosity_pipeline.sh
@@ -1,1 +1,4 @@
-/Users/ddorazio/dev/tools/data-integration/pan.sh -file=/Users/ddorazio/dev/projects/derek-learnosity-pipeline/pdi/learnosity-monthly-payment.ktr -param:school-year-start-date="2016-07-01" -param:day-offset="-1" -param:schema-name="production_skynet" -level=Detailed -log=/Users/ddorazio/dev/projects/derek-learnosity-pipeline/log/learnosity-pdi.log
+TOOL_PATH="/Users/ddorazio/dev/tools"
+PROJECT_PATH="/Users/ddorazio/dev/projects/github/personal/learnosity-pdi-pipeline"
+
+$TOOL_PATH/data-integration/pan.sh -file=$PROJECT_PATH/pdi/learnosity-monthly-payment.ktr -param:school-year-start-date="2016-07-01" -param:day-offset="-1" -param:schema-name="production_skynet" -param:data-path=$PROJECT_PATH/data -level=Detailed -log=$PROJECT_PATH/log/learnosity-pdi.log
